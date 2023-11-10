181eec8575cd7a55b80f53afbd1a8380f32562fb
Wilson-1 <wpcriollo@sudamericano.edu.ec>
2023-11-10 16:35:19 -0500

    Initial commit

---
+++ Ejercicio01/Ejercicio01.js
@@ -0,0 +1,18 @@
+'use strict'
+
+let edad = Number(prompt("Ingrese su edad"));
+let licenciaProfecional = prompt("¿Tiene licencia de cpnducir profecional?");
+
+if(licencia == "Si", licencia == "Si"){
+    licencia = Boolean(true)
+}
+else{
+    licencia = Boolean(false)
+}
+
+if ((age >=25)&&(licencia)){
+    alert("Apto para el puesto");
+}
+else{
+    alert("No apto para el trabajo");
+}
---
+++ Ejercicio02/Ejercicio02.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const: promedio = parseFloat(prompt("Cual es su promedio"));
+const: ingresos = parseInt(prompt("¿Cuales son sus ingresos mensuales?"));
+
+if(prompt >=8.5 && ingresos <=600){
+    alert("Aprobado");
+}
+else{
+    alert("Desaprobado");
+}
